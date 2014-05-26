eaf4b3dfa442cb80160b5196236962faadfdc1a6
jonvallet <j.vallet@gmail.com>
2014-05-26 17:49:01 +0200

    Initial commit

--- src/main/java/com/jonvallet/grosella/GrosellaMenu.java
+++ src/main/java/com/jonvallet/grosella/GrosellaMenu.java
@@ -2,6 +2,9 @@ package com.jonvallet.grosella;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Request;
+import javax.ws.rs.core.SecurityContext;
 
 /**
  * Created by jvalletw on 26/05/14.
@@ -10,8 +13,11 @@ import javax.ws.rs.Path;
 public class GrosellaMenu {
 
     @GET
-    public String getMenus(){
-        return "Hello Grosella!!";
+    public String getMenus(@Context SecurityContext req){
+
+        String user = req.getUserPrincipal().getName();
+
+        return "Hello Grosella user: "+user;
 
     }
 }
